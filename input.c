e2e4dc1a5ee60aa14ad2832effbf81f551f89105
Michelle Massaad <michelle.massaad@epita.fr>
2025-07-23 11:43:02 +0200

    input

---
+++ input.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int age;
+    printf("\nHow old are you? ");
+    fflush(stdout);
+
+    scanf("%d", &age);
+
+    printf("You are %d years old",age);
+
+
+    return 0;
+
+}
+
